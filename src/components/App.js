3182bde2c92d7d98fa7226113a755c60ea1d514a
keisuke <rememberthatk@icloud.com>
2020-04-09 18:12:23 +0900

    [master] setup login context

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,49 +6,65 @@ import {
   Link
 } from "react-router-dom";
 
+import AppContext from '../contexts/AppContext';
+
 import Dashboard from './Dashboard';
 import Header from './Header';
 import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 
+const initialState = { loggedIn: false };
+
 function App() {
+  const [state, setState] = useState(initialState);
+  const renderLinkList = () => {
+    if (state.loggedIn) {
+      return (
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </React.Fragment>
+      );
+    }
+  };
   return (
-    <Router>
-      <div>
-        <Header />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AppContext.Provider value={{ state, setState }}>
+      <Router>
+        <div>
+          <Header />
+          <nav>
+            <ul>
+              {renderLinkList()}
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AppContext.Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 
+import AppContext from '../contexts/AppContext';
+
 const Header = () => {
+  const { state, setState } = useContext(AppContext);
+
   const header = {
     padding: '0 20px',
     height: '60px',
@@ -16,12 +20,31 @@ const Header = () => {
   const loginButton = {
     marginLeft: 'auto',
   };
+  const handleLogin = () => {
+    setState({ loggedIn: true });
+  };
+  const handleLogout = () => {
+    setState({ loggedIn: false });
+  };
+
+  const renderButton = () => {
+    if (state.loggedIn) {
+      return (
+        <button onClick={handleLogout} style={loginButton}>Logout</button>
+      );
+    } else {
+      return (
+        <button onClick={handleLogin} style={loginButton}>Login</button>
+      );
+    }
+  };
+
   return (
     <div style={header}>
       <h1>
         <Link to="/" style={logo}>Login Sample</Link>
       </h1>
-      <button style={loginButton}>Login</button>
+      {renderButton()}
     </div>
   );
 }
