b7c1677e660c610fb0c194086591432d28f8474d
keisuke <rememberthatk@icloud.com>
2020-04-10 08:31:06 +0900

    [master] change component to function

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,44 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
+const SignUp = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(email);
+    console.log(password);
   };
 
-  render() {
-    return (
-      <div>
-        <h2>Sign Up</h2>
-        <form className="white" onSubmit={this.handleSubmit}>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id='email' onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id='password' onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form className="white" onSubmit={handleSubmit}>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id='email' onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id='password' onChange={e => setPassword(e.target.value)} />
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
