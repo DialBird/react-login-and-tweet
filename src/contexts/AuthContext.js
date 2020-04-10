f272c3dc2a950942a592251fcdcf66af2f7353d2
keisuke <rememberthatk@icloud.com>
2020-04-10 09:52:52 +0900

    [master] redirect to dashboard after signin

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
   const login = async (email, password, history) => {
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
-      history.push("/");
+      history.push("/dashboard");
     } catch (error) {
       alert(error);
     }
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
   const signup = async (email, password, history) => {
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, password);
-      history.push("/");
+      history.push("/dashboard");
     } catch (error) {
       alert(error);
     }
