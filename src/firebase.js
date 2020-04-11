618f093db76d3076bf8a05bd01b86bd5de1edf61
keisuke <rememberthatk@icloud.com>
2020-04-11 10:16:58 +0900

    [master] change firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import "firebase/firestore";
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzNQgyyOL2UzZ6boyh3qtorGtWuxDserI",
-  authDomain: "react-myapp-2fe0a.firebaseapp.com",
-  databaseURL: "https://react-myapp-2fe0a.firebaseio.com",
-  projectId: "react-myapp-2fe0a",
-  storageBucket: "react-myapp-2fe0a.appspot.com",
-  messagingSenderId: "588521489489",
-  appId: "1:588521489489:web:e2af5f42105467d8666470",
-  measurementId: "G-XFWKD0RMK9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 firebase.initializeApp(firebaseConfig);
 
