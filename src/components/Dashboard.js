79590d445fa27107722b65ff576d54dc0ec9eba2
keisuke <rememberthatk@icloud.com>
2020-04-11 09:47:27 +0900

    [master] enable to link twitter

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
 
-class Dashboard extends React.Component {
-  render() {
-    return (
-      <div>Dashboard</div>
-    );
-  }
+import AuthContext from '../contexts/AuthContext';
+
+const Dashboard = () => {
+  const history = useHistory();
+  const { loginWithTwitter } = useContext(AuthContext);
+
+  const handleTwitter = () => {
+    loginWithTwitter(history);
+  };
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <button onClick={handleTwitter}>Twitter</button>
+    </div>
+  );
 }
 
 export default Dashboard;
