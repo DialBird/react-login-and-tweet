cd7919a31d1f43387b46a33af314de434b05832f
keisuke <rememberthatk@icloud.com>
2020-04-12 15:27:25 +0900

    [master] enable to set uid

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AuthContext from '../contexts/AuthContext';
 
 const Dashboard = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div>
       <h2>Dashboard</h2>
-      <a href="https://us-central1-facebookloginwithfirebas-e8c65.cloudfunctions.net/api/auth/twitter">Twitter</a>
+      <a href={`https://us-central1-facebookloginwithfirebas-e8c65.cloudfunctions.net/api/auth/twitter?uid=${currentUser.uid}`}>Twitter</a>
     </div>
   );
 }
